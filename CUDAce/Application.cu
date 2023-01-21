02d22701507935fc356cf16bb424a53c63fdf36f
david.chemaly <david.chemaly@epita.fr>
2023-01-21 16:14:58 +0100

    Add project files.

---
+++ CUDAce/Application.cu
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		// TODO: Print usage
+		std::cerr << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	return EXIT_SUCCESS;
+}
